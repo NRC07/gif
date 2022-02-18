61ceab7381e8f0fc4a0de7bc1bd06f087f0a6249
NRC07 <99951211+NRC07@users.noreply.github.com>
2022-02-18 08:57:14 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,75 @@
+basic.showNumber(3)
+basic.showNumber(2)
+basic.showNumber(1)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . # #
+        . . . . #
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # .
+        . . . # #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # .
+        . . . # #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # . .
+        . . # # #
+        . . # . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # . . #
+        . # # # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # . # .
+        # # # . .
+        # # # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        # # . . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . . .
+        # . . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+})
